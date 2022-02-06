4486a04e66036cf9fb354d6d3e5e75fe91c7bbb2
osaguild <nobuhisa.abe.0922@gmail.com>
2022-02-06 21:55:14 +0900

    first commit

---
+++ src/main/kotlin/com/osaguild/worker_sample/WorkerSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.osaguild.worker_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkerSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkerSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/osaguild/worker_sample/WorkerSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.osaguild.worker_sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkerSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
